944453d55c8685fe815edd0af81dd4da1d2ca038
“Laman <hesenlileman2001@gmail.com>
2022-02-27 17:45:04 +0400

    Create content order page

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ const navbar = document.querySelector(".nav__collapse");
 const hamburger = document.querySelector(".hamburger");
 const btnClose = document.querySelector(".nav__close");
 
-hamburger.addEventListener("click", function () {
+hamburger?.addEventListener("click", function () {
   overlay.classList.remove("hidden");
   navbar.style.transform = "translateX(0)";
 });
 
-btnClose.addEventListener("click", function () {
+btnClose?.addEventListener("click", function () {
   overlay.classList.add("hidden");
   navbar.style.transform = "translateX(-100%)";
 });
